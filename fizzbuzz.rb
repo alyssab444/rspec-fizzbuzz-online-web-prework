242cb6586cc1ff58dbeb613f2a66f4f1ad5da69d
Alyssa Brown <alyssa.brown@live.com>
2018-12-19 00:34:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
-def fizzbuzz(int)
+def fizzbuzz (int)
   if int % 3 == 0 
    return "Fizz"
-elseif fizzbuzz(int)
+elseif fizzbuzz (int)
   if int % 5 == 0 
     return "Buzz"
-elseif fizzbuzz(int)
+elseif fizzbuzz (int)
   if int % 3 == 0 && int % 5 == 0 
     return "Fizzbuzz" 
 end 
