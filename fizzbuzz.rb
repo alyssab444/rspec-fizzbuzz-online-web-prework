455e62817e4e46ebe77ee4ca6c4c0c79e8fc43e7
Alyssa Brown <alyssa.brown@live.com>
2018-12-19 01:04:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number) 
   if number % 3 == 0 && number % 5 == 0
-    return "fizzBuzz"
+    return "FizzBuzz"
   elseif number % 3 == 0 
     return "Fizz"
   elseif number % 5 == 0   
